5049267fe8cbe362b4e312d5e539afdb7b6c210f
robbishimozuki <robbishimozuki@gmail.com>
2025-06-27 14:01:14 +0800

    masalah import

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AlternatifController;
+use App\Http\Controllers\Auth\SantriRegisterController;
 use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\KriteriaController;
 use App\Http\Controllers\ObjekController;
